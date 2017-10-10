5a53ee01925da4df210c6b374bef695ce9bb5e97
Unknown <liemnt.uit@gmail.com>
2017-10-10 22:14:50 +0700

    Done

---
+++ stopwatch/App.js
@@ -0,0 +1,180 @@
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    TextInput,
+    TouchableHighlight
+} from 'react-native';
+import formatTime from 'minutes-seconds-milliseconds';
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            timeElapsed: null, //Difference between the current time and the startTime
+            running: false, //Does the clock is ticking?
+            startTime: null, //Record the startTime, when user press start.
+            laps: [], //Array from lap records
+        };
+        this.handleStartPress = this.handleStartPress.bind(this);
+        this.startStopButton = this.startStopButton.bind(this);
+        this.handleLapPress = this.handleLapPress.bind(this);
+    }
+
+    laps() {
+        return this.state.laps.map(function(time, index) {
+            return <View key={index} style={styles.lap}>
+              <Text style={styles.lapText}>
+                Lap #{index + 1}
+              </Text>
+              <Text style={styles.lapText}>
+                  {formatTime(time)}
+              </Text>
+            </View>
+        });
+    }
+    handleReset = () =>{
+        if (this.state.running) {
+            clearInterval(this.interval);
+        }
+        this.setState({
+            timeElapsed: null, //Difference between the current time and the startTime
+            running: false, //Does the clock is ticking?
+            startTime: null, //Record the startTime, when user press start.
+            laps: [], //Array from lap records
+
+        })
+    }
+    resetButton() {
+
+
+        return <TouchableHighlight underlayColor="gray"
+                                   onPress={this.handleReset} style={[styles.stopButton,styles.button,]}>
+            <Text>
+                Reset
+            </Text>
+        </TouchableHighlight>
+    }
+
+
+    startStopButton() {
+        var style = this.state.running ? styles.stopButton : styles.startButton;
+
+        return <TouchableHighlight underlayColor="gray"
+                                   onPress={this.handleStartPress} style={[styles.button, style]}>
+          <Text>
+              {this.state.running ? 'Stop' : 'Start'}
+          </Text>
+        </TouchableHighlight>
+    }
+
+    lapButton() {
+        return <TouchableHighlight style={styles.button}
+                                   underlayColor="gray" onPress={this.handleLapPress}>
+          <Text>
+            Lap
+          </Text>
+        </TouchableHighlight>
+    }
+
+    handleLapPress() {
+        var lap = this.state.timeElapsed;
+
+        this.setState({
+            startTime: new Date(),
+            laps: this.state.laps.concat([lap])
+        });
+    }
+
+    handleStartPress() {
+        if (this.state.running) {
+            clearInterval(this.interval);
+            this.setState({running: false});
+            return
+        }
+
+        this.setState({startTime: new Date()});
+
+        this.interval = setInterval(() => {
+            this.setState({
+                timeElapsed: new Date() - this.state.startTime,
+                running: true
+            });
+        }, 30);
+    }
+
+    render() {
+        return <View style={styles.container}>
+          <View style={styles.header}>
+            <View style={styles.timerWrapper}>
+              <Text style={styles.timer}>
+                  {formatTime(this.state.timeElapsed)}
+              </Text>
+            </View>
+            <View style={styles.buttonWrapper}>
+                {this.lapButton()}
+                {this.startStopButton()}
+                {this.resetButton()}
+            </View>
+          </View>
+          <ScrollView style={styles.footer}>
+              {this.laps()}
+          </ScrollView>
+        </View>
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, // Fill the entire the screen
+        margin: 20
+    },
+    header: {
+        flex: 1
+    },
+    footer: {
+        flex: 1,
+        overflow:'scroll'
+    },
+    timerWrapper: {
+        flex: 5,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonWrapper: {
+        flex: 3,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center'
+    },
+    lap: {
+        justifyContent: 'space-around',
+        flexDirection: 'row',
+        backgroundColor: 'lightgray',
+        padding: 10,
+        marginTop: 10
+    },
+    button: {
+        borderWidth: 2,
+        height: 100,
+        width: 100,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    timer: {
+        fontSize: 60
+    },
+    lapText: {
+        fontSize: 30
+    },
+    startButton: {
+        borderColor: 'green'
+    },
+    stopButton: {
+        borderColor: 'red'
+    }
+});
